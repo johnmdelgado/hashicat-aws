fb4a798c2442ac5c0354d8bb4ade9be72be366c1
John Delgado <johnmdelgado@outlook.com>
2022-03-24 19:41:52 +0000

    updated var

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3_bucket" {
   source  = "app.terraform.io/johndelgado-chip-prep/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket_prefix = var.prefix
+  bucket_prefix = "${var.prefix}"
   bucket = "chip-prep-lab"
   acl    = "private"
 
